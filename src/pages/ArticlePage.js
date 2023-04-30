01726e6e6679fa7e42cbe5b91956307e3a802c16
jerzka <kjerzmanowska@gmail.com>
2023-04-30 18:44:40 -0500

    Connecting backend and frontend
    - installing Axios library in a frontend directory to make an request to the server
    - adding articleInfo state to ArticlePage component using UseState React hook to store the response from the server
    - using useEffect React hook to add logic into component that will be executed outside of the normal component rendering to loading data from the server - in ArticlePage showing number of upvotes
    - adding CommentList component to show comments of the article

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,9 +1,24 @@
+import { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
+import axios from 'axios';
 import NotFoundPage from "./NotFoundPage";
 import articles from './article-content';
+import CommentsList from '../components/CommentsList';
 
 const ArticlePage = () => {
+    const [articleInfo, setArticleInfo] = useState({name: '', upvotes: 0, comments: []});
     const { articleId } = useParams();
+
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`); //because proxy in package.json is setup no need to write http://localhost:8000 - proxy prevent CORS-Origin-Errors
+            const newArticleInfo = response.data;
+            //setArticleInfo({upvotes: Math.ceil(Math.random() * 10), commment: []});
+            setArticleInfo(newArticleInfo);
+        }
+        loadArticleInfo();
+    }, [articleId]);
+    
     const article = articles.find(article => article.name === articleId);
 
     if(!article){
@@ -13,11 +28,14 @@ const ArticlePage = () => {
     return (
         <>
         <h1>{article.title}</h1>
+        <p>This article has { articleInfo.upvotes } vote(s)</p>
         {article.content.map((paragraph, index) => (
             <p key={index}>{paragraph}</p>
         ))}
+        <CommentsList comments={articleInfo.comments} />
         </>
     )
 }
+
 
 export default ArticlePage;
